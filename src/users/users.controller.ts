0267440d561a2b61ab0b656657cae82476913fd4
Eduardo Tavares <edutav@gmail.com>
2020-11-04 12:18:19 -0400

    add autenticação e autorização

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Role } from 'src/auth/role.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { ReturnUserDto } from './dtos/return-user.dto';
+import { UserRole } from './user-roles.enum';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -8,6 +18,8 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
+  @Role(UserRole.ADMIN)
+  @UseGuards(AuthGuard(), RolesGuard)
   async createAdminUser(
     @Body(ValidationPipe) createUserDto: CreateUserDto,
   ): Promise<ReturnUserDto> {
